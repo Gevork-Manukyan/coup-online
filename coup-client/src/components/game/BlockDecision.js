b4ae17ed8e0c212e36454b53e175bce843db846b
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-06-01 18:53:56 -0700

    rewrites BlockDecision to use function component

--- coup-client/src/components/game/BlockDecision.js
+++ coup-client/src/components/game/BlockDecision.js
@@ -1,106 +1,103 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 
-export default class BlockDecision extends Component {
+const BlockDecision = ({ name, socket, doneAction, closeOtherVotes, action, doneBlockVote }) => {
+    const [isDecisionMade, setIsDecisionMade] = useState(false);
+    const [decision, setDecision] = useState('');
+    const [isPickingClaim, setIsPickingClaim] = useState(false);
 
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            isDecisionMade: false,
-            decision: '',
-            isPickingClaim: false,
-            targetAction: ''
-        }
-    }
-
-    chooseAction = (action, target = null) => {
+    const chooseAction = (action, target = null) => {
         const res = {
             action: {
                 action: action,
                 target: target,
-                source: this.props.name
+                source: name
             }
-        }
-        console.log(res)
+        };
+        console.log(res);
         
-        this.props.socket.emit('g-actionDecision', res)
-        this.props.doneAction();
-    }
+        socket.emit('g-actionDecision', res);
+        doneAction();
+    };
 
-    block = (block, claim = null) => {
-        this.props.closeOtherVotes('block')
-        // res.prevAction.action, res.prevAction.target, res.prevAction.source, res.counterAction, res.blockee, res.blocker, res.isBlocking
-        let resClaim
-        if(claim != null) {
+    const block = (block, claim = null) => {
+        closeOtherVotes('block');
+
+        let resClaim;
+        if (claim != null) {
             resClaim = claim;
-        } else if(block === 'block_foreign_aid') {
-            resClaim = 'duke'
-        } else if(block === 'block_assassinate') {
-            resClaim = 'contessa'
+        } else if (block === 'block_foreign_aid') {
+            resClaim = 'duke';
+        } else if (block === 'block_assassinate') {
+            resClaim = 'contessa';
         } else {
-            console.error('unknown claim, line 40')
+            console.error('unknown claim, line 40');
         }
 
         const res = {
-            prevAction: this.props.action,
+            prevAction: action,
             counterAction: {
                 counterAction: block,
                 claim: resClaim,
-                source: this.props.name
+                source: name
             },
-            blockee: this.props.action.source,
-            blocker: this.props.name,
+            blockee: action.source,
+            blocker: name,
             isBlocking: true
-        }
-        console.log(res)
-        this.props.socket.emit('g-blockDecision', res)
-        this.props.doneBlockVote();
-    }
+        };
+        console.log(res);
+        socket.emit('g-blockDecision', res);
+        doneBlockVote();
+    };
 
-    pass = () => {
+    const pass = () => {
         const res = {
-            action: this.props.action,
+            action: action,
             isBlocking: false
-        }
-        console.log(res)
-        this.props.socket.emit('g-blockDecision', res)
-        this.props.doneBlockVote();
-    }
+        };
+        console.log(res);
+        socket.emit('g-blockDecision', res);
+        doneBlockVote();
+    };
 
-    pickClaim = (block) => {
-        this.props.closeOtherVotes('block')
-        this.setState({ decision: block })
-        this.setState({ isPickingClaim: true })
-    }
+    const pickClaim = (block) => {
+        closeOtherVotes('block');
+        setDecision(block);
+        setIsPickingClaim(true);
+    };
 
-    render() {
-        let control = null
-        let pickClaim = null
-        if(!this.state.isPickingClaim) {
-            if(this.props.action.action === 'foreign_aid') {
-                control = <>
-                <p><b>{this.props.action.source}</b> is trying to use Foreign Aid</p>
-                <button onClick={() => this.block('block_foreign_aid')}>Block Foreign Aid</button>
+    let control = null;
+    let pickClaimContent = null;
+
+    if (!isPickingClaim) {
+        if (action.action === 'foreign_aid') {
+            control = (
+                <>
+                    <p><b>{action.source}</b> is trying to use Foreign Aid</p>
+                    <button onClick={() => block('block_foreign_aid')}>Block Foreign Aid</button>
                 </>
-            } else if(this.props.action.action === 'steal') {
-                control = <button onClick={() => this.pickClaim('block_steal')}>Block Steal</button>
-            } else if(this.props.action.action === 'assassinate') {
-                control = <button onClick={() => this.block('block_assassinate')}>Block Assassination</button>
-            }
-        } else {
-            pickClaim = <>
-                <p>To block steal, do you claim Ambassador or Captain?</p>
-                <button onClick={() => this.block(this.state.decision, 'ambassador')}>Ambassador</button>
-                <button onClick={() => this.block(this.state.decision, 'captain')}>Captain</button>
-            </>
+            );
+        } else if (action.action === 'steal') {
+            control = <button onClick={() => pickClaim('block_steal')}>Block Steal</button>;
+        } else if (action.action === 'assassinate') {
+            control = <button onClick={() => block('block_assassinate')}>Block Assassination</button>;
         }
-        
-        return (
+    } else {
+        pickClaimContent = (
             <>
-               {control}
-               {pickClaim}
-               {/* <button onClick={() => this.pass()}>Pass</button> */}
+                <p>To block steal, do you claim Ambassador or Captain?</p>
+                <button onClick={() => block(decision, 'ambassador')}>Ambassador</button>
+                <button onClick={() => block(decision, 'captain')}>Captain</button>
             </>
-        )
+        );
     }
-}
+    
+    return (
+        <>
+            {control}
+            {pickClaimContent}
+            {/* <button onClick={() => pass()}>Pass</button> */}
+        </>
+    );
+};
+
+export default BlockDecision;
