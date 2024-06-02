e6429170baaf8941096d9834e133bfee823ec5cd
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-06-01 20:00:12 -0700

    rewrites ExchangeInfluences to use functional component

--- coup-client/src/components/game/ExchangeInfluences.js
+++ coup-client/src/components/game/ExchangeInfluences.js
@@ -1,41 +1,35 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 
-export default class ExchangeInfluences extends Component {
-    
-    constructor(props) {
-        super(props)
-        this.state = {
-             influences: props.influences,
-             keep: [],
-             totalInf: props.influences.length
-        }
-    }
-    
+function ExchangeInfluences({ name, influences, socket, doneExchangeInfluence }) {
+    const [keep, setKeep] = useState([]);
+    const [totalInf] = useState(influences.length);
 
-    selectInfluence = (index) => {
-        // res.revealedCard, prevaction, counterAction, challengee, challenger, isBlock
-        this.state.keep.push(this.state.influences.splice(index,1)[0])
-        this.setState({ influences: this.state.influences, putBack: this.state.putBack })
-        if(this.state.keep.length === (this.state.totalInf-2)) {
+    const selectInfluence = (index) => {
+        const updatedInfluences = [...influences];
+        const selectedInfluence = updatedInfluences.splice(index, 1)[0];
+        setKeep(prevKeep => [...prevKeep, selectedInfluence]);
+
+        if (keep.length === (totalInf - 2)) {
             const res = {
-                playerName: this.props.name,
-                kept: this.state.keep,
-                putBack: this.state.influences
-            }
-            this.props.socket.emit('g-chooseExchangeDecision', res);
-            this.props.doneExchangeInfluence();
+                playerName: name,
+                kept: keep,
+                putBack: updatedInfluences
+            };
+            socket.emit('g-chooseExchangeDecision', res);
+            doneExchangeInfluence();
         }
-    }
+    };
 
-    render() {
-        const influences = this.state.influences.map((x, index) => {
-            return <button key={index} onClick={() => this.selectInfluence(index)}>{x}</button>
-        })
-        return ( 
-            <div>
-                <p className="DecisionTitle">Choose which influence(s) to keep</p>
-                {influences}
-            </div>
-        )
-    }
+    const influenceButtons = influences.map((influence, index) => (
+        <button key={index} onClick={() => selectInfluence(index)}>{influence}</button>
+    ));
+
+    return (
+        <div>
+            <p className="DecisionTitle">Choose which influence(s) to keep</p>
+            {influenceButtons}
+        </div>
+    );
 }
+
+export default ExchangeInfluences;
