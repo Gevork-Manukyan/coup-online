923f308e8e2170ef0f0f4f4cece3279e55123b24
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-06-01 19:56:26 -0700

    rewrites EventLog to use functional component

--- coup-client/src/components/game/EventLog.js
+++ coup-client/src/components/game/EventLog.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react'
+import React, { useEffect, useRef } from 'react';
 
-export default class EventLog extends Component {
-    
-    render() {
-        return (
-            <div className="EventLogContainer">
-                <p className="bold EventLogTitle">Event Log</p>
-                <div className="EventLogBody">
-                   {this.props.logs.map((x, index) => {
-                        if(index === this.props.logs.length-1){
-                            return <p className="new">{x}</p>
-                        }
-                    return <p>{x}</p>
-                    })}
-                    <div style={{ float:"left", clear: "both" }}
-                        ref={(el) => { this.messagesEnd = el; }}>
-                    </div> 
-                </div>
+function EventLog({ logs }) {
+    const messagesEndRef = useRef(null);
+
+    const scrollToBottom = () => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [logs]);
+
+    return (
+        <div className="EventLogContainer">
+            <p className="bold EventLogTitle">Event Log</p>
+            <div className="EventLogBody">
+                {logs.map((log, index) => (
+                    <p key={index} className={index === logs.length - 1 ? "new" : ""}>
+                        {log}
+                    </p>
+                ))}
+                <div style={{ float: "left", clear: "both" }} ref={messagesEndRef}></div>
             </div>
-        )
-    }
-
-    scrollToBottom = () => {
-        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
-      }
-      
-      componentDidMount() {
-        this.scrollToBottom();
-      }
-      
-      componentDidUpdate() {
-        this.scrollToBottom();
-      }
+        </div>
+    );
 }
+
+export default EventLog;
